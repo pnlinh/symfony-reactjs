c62a670f63c4f79f532b5193154539555a812268
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-10-22 23:25:40 +0700

    child component, callback prop

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import RepLogList from './RepLogList';
 
 export default class RepLogApp extends Component {
     constructor(props) {
@@ -7,6 +8,8 @@ export default class RepLogApp extends Component {
         this.state = {
             highlightedRowId: null
         };
+
+        this.handleRowClick = this.handleRowClick.bind(this);
     }
 
     handleRowClick(repLogId, event) {
@@ -23,12 +26,6 @@ export default class RepLogApp extends Component {
             heart = <span>❤️</span>;
         }
 
-        const repLogs = [
-            {id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5},
-            {id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180},
-            {id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72}
-        ];
-
         return (
             <div className="col-md-7">
                 <h2>Lift Stuff! {heart}</h2>
@@ -42,22 +39,10 @@ export default class RepLogApp extends Component {
                         <th>&nbsp;</th>
                     </tr>
                     </thead>
-                    <tbody>
-                    {
-                        repLogs.map(repLog =>
-                            (
-                                <tr key={repLog.id}
-                                    className={highlightedRowId === repLog.id ? 'info' : ''}
-                                    onClick={(event) => this.handleRowClick(repLog.id, event)}
-                                >
-                                    <td>{repLog.itemLabel}</td>
-                                    <td>{repLog.reps}</td>
-                                    <td>{repLog.totalWeightLifted}</td>
-                                    <td>...</td>
-                                </tr>
-                            ))
-                    }
-                    </tbody>
+                    <RepLogList
+                        highlightedRowId={highlightedRowId}
+                        onRowClick={this.handleRowClick}
+                    />
                     <tfoot>
                     <tr>
                         <td>&nbsp;</td>
