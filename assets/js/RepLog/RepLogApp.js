7df4f3a087f50d73291edd76a0f903730f7e5c8f
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-10-22 16:30:31 +0700

    props

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 
 export default class RepLogApp extends Component {
     render() {
-        return <h2>Lift History! <span>❤️</span></h2>;
+        let heart = '';
+
+        if (this.props.withHeart) {
+            heart = <span>❤️</span>;
+        }
+
+        return (
+            <h2>Lift History! {heart}</h2>
+        );
     }
 }
--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -2,4 +2,12 @@ import React from 'react';
 import {render} from 'react-dom';
 import RepLogApp from './RepLog/RepLogApp';
 
-render(<RepLogApp/>, document.getElementById('lift-stuff-app'));
+const shouldShowHeart = true;
+
+render(
+    <div>
+        <RepLogApp withHeart={shouldShowHeart} />,
+        <RepLogApp withHeart={false} />,
+    </div>,
+    document.getElementById('lift-stuff-app')
+);
