5df6ce1e8fc68094236fadd1b81ffa45d9d5b164
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-10-22 17:11:57 +0700

    rendering table

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -8,8 +8,49 @@ export default class RepLogApp extends Component {
             heart = <span>❤️</span>;
         }
 
+        const repLogs = [
+            {id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5},
+            {id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180},
+            {id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72}
+        ];
+
+        const repLogElements = repLogs.map(repLog => {
+            return (
+                <tr key={repLog.id}>
+                    <td>{repLog.itemLabel}</td>
+                    <td>{repLog.reps}</td>
+                    <td>{repLog.totalWeightLifted}</td>
+                    <td>...</td>
+                </tr>
+            );
+        });
+
         return (
-            <h2>Lift History! {heart}</h2>
+            <div className="col-md-7">
+                <h2>Lift History! {heart} </h2>
+
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>What</th>
+                        <th>How many times?</th>
+                        <th>Weight</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {repLogElements}
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <th>Total</th>
+                        <th>TODO</th>
+                        <td>&nbsp;</td>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
         );
     }
 }
--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -7,7 +7,6 @@ const shouldShowHeart = true;
 render(
     <div>
         <RepLogApp withHeart={shouldShowHeart} />,
-        <RepLogApp withHeart={false} />,
     </div>,
     document.getElementById('lift-stuff-app')
 );
