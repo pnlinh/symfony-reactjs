41dbff5847ecbd71cc9330374db9726f0e978b94
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-10-22 15:20:59 +0700

    react create element

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,13 @@
-console.log('Hello ReactJs');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = React.createElement(
+    'h2',
+    null,
+    'Lift History!',
+    React.createElement('span', null, '❤️')
+);
+
+console.log(el);
+
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
