ce4250d8a0be99c0740e804e5a4952c45c530401
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-10-25 17:40:32 +0700

    remove proptypes on production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var Encore = require('@symfony/webpack-encore');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
-    // the project directory where all compiled assets will be stored
+// the project directory where all compiled assets will be stored
     .setOutputPath('public/build/')
 
     // the public path used by the web server to access the previous directory
@@ -19,7 +19,7 @@ Encore
 
     .addPlugin(new CopyWebpackPlugin([
         // copies to {output}/static
-        { from: './assets/static', to: 'static' }
+        {from: './assets/static', to: 'static'}
     ]))
 
     .enableSassLoader()
@@ -27,6 +27,13 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableVersioning(Encore.isProduction())
     .enableReactPreset()
+    .configureBabel(babelConfig => {
+        if (Encore.isProduction()) {
+            babelConfig.plugins.push(
+                'transform-react-remove-prop-types'
+            );
+        }
+    })
 ;
 
 // export the final configuration
